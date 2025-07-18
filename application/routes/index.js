6b1539cb9a52fbb7477a210ba0ac410f19c391fa
acruzgarcia21 <alejandrocruzgarcia111@gmail.com>
2025-07-18 11:58:10 -0700

    Updated styling and added .hbs files

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Alejandro Cruz-Garcia" });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'CSC 317 App' });
 });
+
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/post', function (req, res, next) {
+  res.render('post', { title: 'Post' });
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+router.get('/viewpost', function (req, res, next) {
+  res.render('viewpost', { title: 'View Post' });
+});
+
 
 module.exports = router;
